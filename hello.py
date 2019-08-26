6dcc6bf17134ff80f885455a02e677aa9223ff6b
Evgeniy Tortumashev <mocart1567@gmail.com>
2019-08-26 09:54:16 +0700

    Добавил настройки тестов настройки lint исправил замечания lint

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 msg = "Hello World"
-print(msg)
+print (msg)
+import xmlschema
+from pprint import pprint
+my_schema = xmlschema.XMLSchema('./exampleXsd/test.xsd')
+#pprint(my_schema.to_dict('./exampleXsd/test.xml'))
+pprint(dict(my_schema.elements)["SetStateOperDateEKSRs"])
